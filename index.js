c55961ece865deb29d68fcfa09a4e69a9966a6cd
Zhou Ziheng <zhou.ziheng@outlook.com>
2022-02-08 10:45:38 +0800

    Set up MongoDB

--- index.js
+++ index.js
@@ -2,15 +2,34 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Gym = require('./models/gym');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/RateMyGym')
+    .then(() => {
+        console.log("Connected to mongodb");
+    })
+    .catch(error => {
+        console.log("Connection to mongodb failed");
+        console.log(error);
+    });
 
 
 
 // *** ROUTING ***
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/makegym', async (req, res) => {
+    const gym = new Gym({ title: 'Test Gym', description: 'Description for test gym' });
+    await gym.save();
+    res.send('Made new gym...');
 });
 
 
