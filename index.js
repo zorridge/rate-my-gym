f6a9944de1de54cfee271c80138c795a64372475
Zhou Ziheng <zhou.ziheng@outlook.com>
2022-02-08 14:54:29 +0800

    Implement basic CRUD structure

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const Gym = require('./models/gym');
 
@@ -9,6 +10,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://localhost:27017/RateMyGym')
     .then(() => {
@@ -23,13 +25,50 @@ mongoose.connect('mongodb://localhost:27017/RateMyGym')
 
 // *** ROUTING ***
 app.get('/', (req, res) => {
-    res.render('home');
+    res.redirect('/gyms');
 });
 
-app.get('/makegym', async (req, res) => {
-    const gym = new Gym({ title: 'Test Gym', description: 'Description for test gym' });
-    await gym.save();
-    res.send('Made new gym...');
+app.get('/gyms', async (req, res) => {
+    const gyms = await Gym.find({});
+    res.render('gyms/index', { gyms });
+});
+
+// Create new gym
+app.get('/gyms/new', (req, res) => {
+    res.render('gyms/new');
+});
+
+app.post('/gyms', async (req, res) => {
+    const gymNew = new Gym(req.body.gym);
+    await gymNew.save();
+    res.redirect(`/gyms/${gymNew._id}`);
+});
+
+// Read gym information
+app.get('/gyms/:id', async (req, res) => {
+    const { id } = req.params;
+    const gym = await Gym.findById(id);
+    res.render('gyms/show', { gym });
+});
+
+// Update gym information
+app.get('/gyms/:id/edit', async (req, res) => {
+    const gym = await Gym.findById(req.params.id);
+    res.render('gyms/edit', { gym });
+});
+
+app.put('/gyms/:id', async (req, res) => {
+    const { id } = req.params;
+    const gymUpdate = await Gym.findByIdAndUpdate(id, { ...req.body.gym });
+    // res.send(req.body.gym);
+    res.redirect(`/gyms/${gymUpdate._id}`);
+});
+
+// Delete gym
+app.delete('/gyms/:id', async (req, res) => {
+    const { id } = req.params;
+    await Gym.findByIdAndDelete(id);
+    res.redirect('/gyms');
 });
 
 
