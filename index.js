7f1d20dd2312887011c2852876b99e7e6e723831
Zhou Ziheng <zhou.ziheng@outlook.com>
2022-02-10 18:08:01 +0800

    Add async error handler and error template

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const morgan = require('morgan');
+
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const Gym = require('./models/gym');
 
 app.set('view engine', 'ejs');
@@ -32,52 +35,58 @@ app.get('/', (req, res) => {
     res.redirect('/gyms');
 });
 
-app.get('/gyms', async (req, res) => {
+app.get('/gyms', catchAsync(async (req, res) => {
     const gyms = await Gym.find({});
     res.render('gyms/index', { gyms });
-});
+}));
 
 // Create new gym
 app.get('/gyms/new', (req, res) => {
     res.render('gyms/new');
 });
 
-app.post('/gyms', async (req, res) => {
+app.post('/gyms', catchAsync(async (req, res) => {
     const gymNew = new Gym(req.body.gym);
     await gymNew.save();
     res.redirect(`/gyms/${gymNew._id}`);
-});
+}));
 
 // Read gym information
-app.get('/gyms/:id', async (req, res) => {
+app.get('/gyms/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const gym = await Gym.findById(id);
     res.render('gyms/show', { gym });
-});
+}));
 
 // Update gym information
-app.get('/gyms/:id/edit', async (req, res) => {
+app.get('/gyms/:id/edit', catchAsync(async (req, res) => {
     const gym = await Gym.findById(req.params.id);
     res.render('gyms/edit', { gym });
-});
+}));
 
-app.put('/gyms/:id', async (req, res) => {
+app.put('/gyms/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const gymUpdate = await Gym.findByIdAndUpdate(id, { ...req.body.gym });
     // res.send(req.body.gym);
     res.redirect(`/gyms/${gymUpdate._id}`);
-});
+}));
 
 // Delete gym
-app.delete('/gyms/:id', async (req, res) => {
+app.delete('/gyms/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Gym.findByIdAndDelete(id);
     res.redirect('/gyms');
+}));
+
+// Error handler
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found...', 404));
 });
 
-// 404
-app.use((req, res) => {
-    res.status(404).send('404 PAGE NOT FOUND... :(');
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something went wrong...';
+    res.status(statusCode).render('error', { err, statusCode });
 });
 
 
