8c0352462a88e992bab5c97fb1c21c970ba175dd
Zhou Ziheng <zhou.ziheng@outlook.com>
2022-03-26 16:31:38 +0800

    Set up express-session

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const morgan = require('morgan');
+const session = require('express-session');
 
 const ExpressError = require('./utils/ExpressError');
 const gyms = require('./routes/gym');
@@ -14,11 +15,23 @@ const reviews = require('./routes/review');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
+
+const sessionConfig = {
+    secret: 'thisisasecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+};
+app.use(session(sessionConfig));
 
 mongoose.connect('mongodb://localhost:27017/RateMyGym')
     .then(() => {
