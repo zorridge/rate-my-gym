09ab7f22df2aa785db04eda4de9938c681a4ad30
Zhou Ziheng <zhou.ziheng@outlook.com>
2022-03-30 17:46:39 +0800

    Add authentication middleware

--- routes/gym.js
+++ routes/gym.js
@@ -5,6 +5,7 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const Gym = require('../models/gym');
 const { gymSchema } = require('../schemaValidation');
+const { isLoggedIn } = require('../middleware');
 
 
 // *** MIDDLEWARES ***
@@ -26,11 +27,11 @@ router.get('/', catchAsync(async (req, res) => {
 }));
 
 // Create new gym
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('gyms/new');
 });
 
-router.post('/', validateGym, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateGym, catchAsync(async (req, res) => {
     const gymNew = new Gym(req.body.gym);
     await gymNew.save();
     req.flash('success', 'Successfully added a new gym!');
@@ -49,7 +50,7 @@ router.get('/:id', catchAsync(async (req, res) => {
 }));
 
 // Update gym information
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const gym = await Gym.findById(req.params.id);
     if (!gym) {
         req.flash('error', 'Gym not found!');
@@ -58,7 +59,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
     res.render('gyms/edit', { gym });
 }));
 
-router.put('/:id', validateGym, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateGym, catchAsync(async (req, res) => {
     const { id } = req.params;
     const gymUpdate = await Gym.findByIdAndUpdate(id, { ...req.body.gym });
     req.flash('success', 'Successfully updated gym!');
@@ -66,7 +67,7 @@ router.put('/:id', validateGym, catchAsync(async (req, res) => {
 }));
 
 // Delete gym
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Gym.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted gym!');
