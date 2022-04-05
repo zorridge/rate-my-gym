808040090eaf1ac1747aaba9a2cfb1b2a26fd8c6
Zhou Ziheng <zhou.ziheng@outlook.com>
2022-04-06 01:05:55 +0800

    Update seed
    Add Cloudinary url

--- seeds/index.js
+++ seeds/index.js
@@ -22,7 +22,16 @@ const seedDB = async () => {
         const randPrice = Math.floor(Math.random() * 30) + 80;
         const gym = new Gym({
             title: `${sample(firstName)} ${sample(lastName)}`,
-            image: 'https://source.unsplash.com/collection/10552289',
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/zorridge/image/upload/v1649169118/RateMyGym/l7r2d7xuosy92ypbimed.jpg',
+                    filename: 'RateMyGym/l7r2d7xuosy92ypbimed',
+                },
+                {
+                    url: 'https://res.cloudinary.com/zorridge/image/upload/v1649169119/RateMyGym/lym0kxqnyryuy58glt6l.jpg',
+                    filename: 'RateMyGym/lym0kxqnyryuy58glt6l',
+                }
+            ],
             price: randPrice,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque numquam quas veniam iure itaque odit dolorum impedit incidunt qui dolore. Obcaecati, amet nisi dolorem, quae ullam nulla officiis quaerat fugit voluptate rem, doloremque expedita labore? Facere esse quaerat non quia ipsum recusandae, dolor officia ullam ratione blanditiis eius illum deserunt cupiditate odio eum, fugiat asperiores. Quasi atque animi neque ipsam asperiores. Veniam rem eaque, ullam, minus possimus ratione suscipit rerum libero quod officia labore id debitis deserunt quos? Quisquam tempore fugit rerum possimus, voluptatem quas voluptatibus velit itaque quidem est vitae maiores voluptas quam laudantium alias officia praesentium quis nihil.',
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
