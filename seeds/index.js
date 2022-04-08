9c8709a535e457b4996f71b561127261c99ba513
Zhou Ziheng <zhou.ziheng@outlook.com>
2022-04-08 18:35:47 +0800

    Add custom popups to cluster map
    Popups will display HTML added to GymSchema via a virtual

--- seeds/index.js
+++ seeds/index.js
@@ -17,7 +17,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Gym.deleteMany({}); // DELETES EVERYTHING !!!
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 200; i++) {
         const rand1000 = Math.floor(Math.random() * 1000);
         const randPrice = Math.floor(Math.random() * 30) + 80;
         const gym = new Gym({
