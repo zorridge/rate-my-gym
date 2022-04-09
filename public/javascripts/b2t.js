fba0b36bf6655c7444070e75300b4a2d85c7f6dd
Zhou Ziheng <zhou.ziheng@outlook.com>
2022-04-09 21:05:53 +0800

    Add back to top button on index page

---
+++ public/javascripts/b2t.js
@@ -0,0 +1,23 @@
+const mybutton = document.querySelector('#b2t');
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
