1840abe0f43376090630b6a7b3bc0776c9cd016a
Zhou Ziheng <zhou.ziheng@outlook.com>
2022-04-13 16:12:27 +0800

    Add error page

---
+++ public/javascripts/error.js
@@ -0,0 +1,15 @@
+// Modified from https://codepen.io/theyve/pen/zpxrLG
+
+const visual = document.getElementById("visual");
+const events = ['resize', 'load'];
+
+events.forEach(function (e) {
+    window.addEventListener(e, function () {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        const ratio = 45 / (width / height);
+        visual.style.transform = "translate(-50%, -50%) rotate(-" + ratio + "deg)";
+    });
+});
+
+
